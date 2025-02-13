227679655dd435fb99d8246fbfb27917d444c01c
Gunjan Bholane <152423360+GunjanBholane@users.noreply.github.com>
2025-02-13 19:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const startButton = document.getElementById('startButton');
+
+    startButton.addEventListener('click', function() {
+        // Replace 'https://example.com/proj-link2' with your actual project link
+        window.location.href = 'https://example.com/proj-link2';
+    });
+});
